7f45946395487132af8dffed427b090b92ed9e7c
ATHARVA <atharvapundkar002@gmail.com>
2026-08-01 17:58:18 +0530

    Time: 15 ms (86.47%) | Memory: 12.5 MB (85.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        freq = {}
+        count = 0
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        
+        for ch in t:
+            if ch not in freq:
+                return False
+            freq[ch] -= 1
+            if freq[ch] < 0:
+                return False
+        return True
+                
+            
+   
+       
+
+        
